defbae5145fbf8135efeba9c7275a5cc1e9172ba
g4bri0s <g.cristianods@gmail.com>
2023-08-18 16:21:11 -0300

    feat: leacture 2

---
+++ lecture2/diffOS.java
@@ -0,0 +1,15 @@
+package lecture2;
+
+import java.util.Scanner;
+
+public class diffOS {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Trying to get values from console...");
+
+        System.out.print(scanner.nextLine());
+
+        scanner.close();
+    }
+}
